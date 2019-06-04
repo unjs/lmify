8f8b4bba878c515face6206c1a639b05ffd8825e
HANATANI Takuma <mail@potato4d.me>
2019-06-04 20:55:15 +0900

    test: Write uninstall command test

---
+++ test/uninstall.test.js
@@ -0,0 +1,25 @@
+import path from 'path'
+import fs from 'fs-extra'
+import LMIFY from '../src/index'
+
+for (const name of ['npm', 'yarn']) {
+  test('Install with ' + name, async () => {
+    const rootDir = path.join(__dirname, 'fixtures', name)
+
+    const lmify = new LMIFY({
+      rootDir,
+      stdout: 'ignore',
+      stderr: 'ignore'
+    })
+
+    await lmify.install(['is-nan', 'std-env'])
+
+    expect(await fs.exists(path.join(rootDir, 'node_modules', 'is-nan'))).toBe(true)
+    expect(await fs.exists(path.join(rootDir, 'node_modules', 'std-env'))).toBe(true)
+
+    await lmify.uninstall(['is-nan', 'std-env'])
+
+    expect(await fs.exists(path.join(rootDir, 'node_modules', 'is-nan'))).toBe(false)
+    expect(await fs.exists(path.join(rootDir, 'node_modules', 'std-env'))).toBe(false)
+  }, 60000)
+}
